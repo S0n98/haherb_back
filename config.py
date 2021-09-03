174d5cbd45f342a1220325c9411aa0c06b0f4e47
S0n98 <62588093+S0n98@users.noreply.github.com>
2021-09-03 22:03:49 +0700

    Delete credential info

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'postgresql://postgres:Anhungu2010@localhost/haweb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ITEMS_PER_PAGE = 10
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
